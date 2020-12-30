57f477bcb4dec42ed2416dc0e46240951354ec43
Koki Yanagawa <redd0u610v6spn23@gmail.com>
2020-12-30 11:52:46 +0900

    chap4 演習

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -9,12 +9,7 @@ RSpec.describe Note, type: :model do
   end
 
   before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
+    @user = FactoryBot.create(:user)
 
     @project = @user.projects.create(
       name: "Test Project",
