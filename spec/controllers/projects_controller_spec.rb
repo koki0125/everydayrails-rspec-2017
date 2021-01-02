9d068c6c8410874ac4e3f6ce7d18b12e0c0ce1f4
Koki Yanagawa <redd0u610v6spn23@gmail.com>
2021-01-02 12:16:54 +0900

    演習1完了

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -248,32 +248,69 @@ RSpec.describe ProjectsController, type: :controller do
   end
 
   describe "#new" do
-    # 認可されたユーザーとして
-    context "as an authorized user" do
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do
+
       # 正常にレスポンスを返すこと
       it "responds successfully" do
         sign_in @user
-        get :new, params: {id: @user.id}
+        get :new
+        expect(response).to be_success
+      end
+      
+      # 200レスポンスを返すこと
+      it "returns a 200 response" do
+        sign_in @user
+        get :new
+        expect(response).to have_http_status "200" 
+      end
+    end
+
+    # ゲストとして
+    context "as a guest" do
+      # 302レスポンスを返すこと
+      it "returns a 302 response" do
+        get :new
+        expect(response).to have_http_status "302" 
+      end
+
+      # サインイン画面にリダイレクトすること
+      it "redirects to the sign-in page" do
+        get :new
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
+  describe "#edit" do
+    # 認可されたユーザーとして
+    context "as an authorized user" do
+      before do
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      # 正常にレスポンスを返すこと
+      it "response successfully" do
+        sign_in @user
+        get :edit, params: {id: @project.id}
         expect(response).to be_success
       end
     end
 
     # 認可されていないユーザーとして
-    # context "as an unauthorized user" do
-    #   before do
-    #     other_user = FactoryBot.create(:user)
-    #     sign_in other_user
-    #   end
-    #   # ダッシュボードにリダイレクトすること
-    #   it "redirects to the dashboard" do
-    #     get :new, params: {user_id: @user.id}
-    #     expect(response).to redirect_to root_path
-    #   end
-    # end
-  end
+    context "as an unauthorized user" do
+      before do
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
 
-  describe "#edit" do
-    
+      # ダッシュボードにリダイレクトすること
+      it "redirects to the dashboard" do
+        sign_in @user
+        get :edit, params: {id: @project.id}
+        expect(response).to redirect_to root_path
+      end
+    end
   end
   
   
