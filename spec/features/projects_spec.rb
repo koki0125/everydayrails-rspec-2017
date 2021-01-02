0c5bd43d3fa36505025966850afe8d927cb3e68f
Koki Yanagawa <redd0u610v6spn23@gmail.com>
2021-01-02 13:25:17 +0900

    addv gem launchy

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Projects", type: :feature do
+
+  # ユーザーは新しいプロジェクトを作成する
+  scenario "user creates a new project" do
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    exoect {
+      click_link "New project"
+      fill_in "Name", with: "the project"
+      fill_in "Description", with: "trying out capybara"
+      click_button "Create Project"
+
+      expect(page).to have_content "Project was successfully created"
+      expect(page).to have_content "the project"  
+      expect(page).to have_content "Owner: #{user.name}"  
+    }.to change(user.projects, :count).by(1)
+  end
+
+end
